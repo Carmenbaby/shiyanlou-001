0537e02156f13c80de0c313176efa1059ddbf7c8
kamengbaby <535537502@qq.com>
2019-03-03 22:44:45 +0800

    scan.py

--- scan.py
+++ scan.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 
+import sys
 import getopt
 from multiprocessing import Process
-from socket import *
+import socket
 
 class Args():
     def __init__(self):
         #host '220.181.57.216' // port ['80','85'] 
         self._host, self._port = self._get_args()
+        #print(self._host)
+        #print(self._port)
     
-    def _get_args(self)
+    def _get_args(self):
         opts,_  = getopt.getopt(sys.argv[1:],'',['host=','port='])
         
         for opt_name,opt_value in opts:
             if opt_name in ['--host']:
+            
                 host = opt_value
-            if opt_name in ['--port']
+            if opt_name in ['--port']:
                 port = opt_value.split('-')
         return host,port
     
@@ -32,21 +36,25 @@ args = Args()
 class Scanf(Process):
     
     def __init__(self):
-        super().__init__()
-        
+        super().__init__() 
+
     def run(self):
         #arg.port_path[0] arg.port_path[1]端口起始截止
-        for port in range(arg.port_path[0],arg.port_path[1])：
-            address_tuple = (arg.host_path,port)
+        start_port = int(args.port_path[0])
+        end_port =int(args.port_path[1])
+
+        for port in range(start_port,end_port):
+            address_tuple = (args.host_path,port)
             tcpSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcpSock.settimeout(0.1)
             try:
                 tcpSock.connect(address_tuple)
+                print('{} open'.format(port))
             except socket.error:
                 print('{} closed'.format(port))
             
             tcpSock.close()
-            print('{} open'.format(port)
-
+           
 def main():
 
     scanf = Scanf()
