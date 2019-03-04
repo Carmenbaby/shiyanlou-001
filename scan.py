ffbb5347ca2fcbdd55ba5fd972204e45909efd5a
Your Name <you@example.com>
2019-03-04 20:31:14 +0800

    scan.py

--- scan.py
+++ scan.py
@@ -4,6 +4,7 @@ import sys
 import getopt
 from multiprocessing import Process
 import socket
+import re
 
 class Args():
     def __init__(self):
@@ -15,12 +16,21 @@ class Args():
     def _get_args(self):
         opts,_  = getopt.getopt(sys.argv[1:],'',['host=','port='])
         
+        host = ''
+        port = []
+
         for opt_name,opt_value in opts:
             if opt_name in ['--host']:
-            
                 host = opt_value
+                if re.findall('\d+.\d+.\d+.\d+',host) == []:
+                    print('Parameter Error')
+                    sys.exit(1)
             if opt_name in ['--port']:
-                port = opt_value.split('-')
+                if  '-' in opt_value:
+                    port = opt_value.split('-')
+                else:
+                    port = [opt_value,opt_value]
+                   # print(port)
         return host,port
     
     @property
@@ -39,12 +49,13 @@ class Scanf(Process):
         super().__init__() 
 
     def run(self):
-        #arg.port_path[0] arg.port_path[1]端口起始截止
+        #arg.port_path[0] arg.port_path[1]
         start_port = int(args.port_path[0])
         end_port =int(args.port_path[1])
 
-        for port in range(start_port,end_port):
+        for port in range(start_port,end_port+1):
             address_tuple = (args.host_path,port)
+            #print(address_tuple)
             tcpSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             tcpSock.settimeout(0.1)
             try:
